8671a2cf55b6e76fcc26b0aef666657203debbcc
beike98 <49637524+beike98@users.noreply.github.com>
2021-09-15 12:55:21 +0800

    PC端添加提示

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
-<div>
-  <router-view></router-view>
-  <TabBar></TabBar>
-</div>
+  <div>
+    <router-view></router-view>
+    <IsPC v-if="isPC"></IsPC>
+    <TabBar></TabBar>
+  </div>
 </template>
 <script>
 import TabBar from "@/components/TabBar";
+import IsPC from "@/components/IsPC";
 export default {
-  components: {TabBar}
-}
+  components: { TabBar, IsPC },
+  data() {
+    return {
+      isPC: false,
+    };
+  },
+  mounted() {
+    if (document.body.clientWidth > 500) {
+      this.isPC = true;
+    }
+  },
+};
 </script>
 <style>
-body{
+body {
   background: #f7f8fa;
 }
 </style>
---
+++ src/components/IsPC.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>为了您更好的体验本APP，</p>
+    <p>请使用移动端设备扫码预览：</p>
+    <img src="../assets/2wm.png" alt="二维码" />
+  </div>
+</template>
+<style scoped>
+div {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-top: -200px;
+  margin-left: -128px;
+  z-index: 20;
+}
+</style>
